beefbae781b95c919862dd239e6b782579b01b5e
zzanette <alysson.zanette@hotmail.com>
2018-04-02 22:15:48 -0300

    Added first commit to NodeJs server and mongodb connection.

---
+++ server/config.js
@@ -0,0 +1,9 @@
+const db = 'ADD YOUR URL HERE!!';
+const port = 3030;
+const secret_key = 'ADD YOUR SECRET KEY HERE!!';
+
+module.exports = {
+    database: db,
+    port: port,
+    secret_key: secret_key
+}
---
+++ server/server.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const monrgan = require('morgan');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+
+const config = require('./config');
+
+const app = express();
+
+mongoose.connect(config.database, (err) => {
+    if (err) {
+        console.log(err);
+    } else {
+        console.log("Connected!!!!");
+    }
+})
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(monrgan('dev'));
+app.use(cors)
+
+app.get('/', (req, res, next) => {
+    res.json({
+        test: 'hello'
+    });
+});
+
+app.listen(config.port, err => {
+    console.log('magic: ' + config);
+})
+
